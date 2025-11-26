d94b993dc5419be58a33e4b6033e6a16bbdab3aa
Vlad Mehakovic <vlad@Vlads-MacBook-Pro.local>
2025-11-26 13:22:30 +1100

    test: remove untestable railtie skip test

--- test/mbuzz/railtie_test.rb
+++ test/mbuzz/railtie_test.rb
@@ -6,9 +6,4 @@ class Mbuzz::RailtieTest < Minitest::Test
   def test_railtie_file_exists
     assert File.exist?(File.expand_path("../../lib/mbuzz/railtie.rb", __dir__))
   end
-
-  def test_railtie_loads_when_rails_available
-    skip "Rails not available - Railtie only loads in Rails environment"
-    assert defined?(Mbuzz::Railtie)
-  end
 end
